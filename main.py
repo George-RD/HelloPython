c3f8694da1e7cdd780461a8532ffd2d0f46fdc08
George-RD <180932462+George-RD@users.noreply.github.com>
2025-05-12 10:01:24 +0400

    Main.py edit

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ y = 5
 print(x + y)
 x="fugro"
 print(x)
+x = 10
+print("Subtraction is: ", x, " - ", y, " = ", x - y)
+print("Multiplication is: ", x, " * ", y, " = ", x * y)
+print("Addition is: ", x, " + ", y, " = ", x + y)
+print("Division is: ", x, " / ", y, " = ", x / y)
+
