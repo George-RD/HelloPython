b7eaab1a69cc4ab1f5530a4e15aa036e18f39e75
George-RD <180932462+George-RD@users.noreply.github.com>
2025-05-18 15:25:51 +0400

    Moved Files

---
+++ PythonBootcamp/main.py
@@ -0,0 +1,12 @@
+print("HelloWorld!")
+x = 10
+y = 5
+print(x + y)
+x="fugro"
+print(x)
+x = 10
+print("Subtraction is: ", x, " - ", y, " = ", x - y)
+print("Multiplication is: ", x, " * ", y, " = ", x * y)
+print("Addition is: ", x, " + ", y, " = ", x + y)
+print("Division is: ", x, " / ", y, " = ", x / y)
+
